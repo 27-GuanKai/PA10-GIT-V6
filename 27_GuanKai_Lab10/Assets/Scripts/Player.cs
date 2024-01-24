e5a7becb2378a9eea544cc664da90d16f78948b3
AhhhhYe <hiddendreams3112@gmail.com>
2024-01-24 11:47:15 +0800

    Finished Game

---
+++ 27_GuanKai_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player : MonoBehaviour
+{
+    private float PosY;
+    private float xMin = -3.96f, xMax = 3.96f;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float PosY = this.transform.position.y;
+        float verticalInput = Input.GetAxis("Vertical");
+        this.transform.position =new Vector3(transform.position.x, Mathf.Clamp(PosY, xMin, xMax));
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ 27_GuanKai_Lab10/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour
+{
+    public Text ScoreText;
+    float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            score += 1;
+            ScoreText.GetComponent<Text>().text = ("Score: " + score);
+            Destroy(other.gameObject);
+        }
+    }
+}
